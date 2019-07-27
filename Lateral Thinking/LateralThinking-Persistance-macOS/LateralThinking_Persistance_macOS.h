f962bba254971a0dddf9f5239ec3c2724a64a887
Paul Wood III <paul.wood.iii@gmail.com>
2019-07-27 13:33:00 -0400

    add frameworks for persistance of tvOS and macOS

---
+++ Lateral Thinking/LateralThinking-Persistance-macOS/LateralThinking_Persistance_macOS.h
@@ -0,0 +1,19 @@
+//
+//  LateralThinking_Persistance_macOS.h
+//  LateralThinking-Persistance-macOS
+//
+//  Created by Paul Wood on 7/27/19.
+//  Copyright © 2019 Paul Wood. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LateralThinking_Persistance_macOS.
+FOUNDATION_EXPORT double LateralThinking_Persistance_macOSVersionNumber;
+
+//! Project version string for LateralThinking_Persistance_macOS.
+FOUNDATION_EXPORT const unsigned char LateralThinking_Persistance_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LateralThinking_Persistance_macOS/PublicHeader.h>
+
+
---
+++ Lateral Thinking/LateralThinking-Persistance-tvOS/LateralThinking_Persistance_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  LateralThinking_Persistance_tvOS.h
+//  LateralThinking-Persistance-tvOS
+//
+//  Created by Paul Wood on 7/27/19.
+//  Copyright © 2019 Paul Wood. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LateralThinking_Persistance_tvOS.
+FOUNDATION_EXPORT double LateralThinking_Persistance_tvOSVersionNumber;
+
+//! Project version string for LateralThinking_Persistance_tvOS.
+FOUNDATION_EXPORT const unsigned char LateralThinking_Persistance_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LateralThinking_Persistance_tvOS/PublicHeader.h>
+
+
